2c711893b343c4d32874d03a6176967ebe0fefc6
Keazz1342 <namkea669@gmail.com>
2026-07-19 22:07:41 +0700

    update db.js to connect aiven

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,16 @@ import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'GAMS',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
   namedPlaceholders: true
@@ -14,6 +19,7 @@ const pool = mysql.createPool({
 
 export async function testConnection() {
   const connection = await pool.getConnection();
+  console.log("✅ Connected to Aiven MySQL");
   connection.release();
 }
 
